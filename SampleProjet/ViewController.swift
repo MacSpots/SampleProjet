a68dcab4e13a3a1f03558edb2d63a57df78a4555
Jason Jardim <jsquared@macspots.com>
2019-10-11 10:35:28 -0400

    Initial Commit

---
+++ SampleProjet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProjet
+//
+//  Created by Jason Jardim on 10/11/19.
+//  Copyright © 2019 Jason Jardim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
